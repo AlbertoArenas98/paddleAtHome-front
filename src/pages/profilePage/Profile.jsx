e39c706978cc3c4f27d4c01fd08ab2e267947d88
Alberto <albertoarennass@gmail.com>
2022-09-27 12:01:51 +0200

    feat:user-profile

--- src/pages/profilePage/Profile.jsx
+++ src/pages/profilePage/Profile.jsx
@@ -2,18 +2,22 @@ import "../profilePage/profile.scss"
 import img from "../../assets/Logo con tipografía circular para servicios fotógrafo beis y negro.png"
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import { useGetter } from "../../store";
 
 export default function Profile() {
+
+    const {user} = useGetter();
+
+    console.log(user)
 
     return (
 
         <div className="profilePage">
         <form className="authpage-container_form">
             <img src={img} alt="" />
-            <h1>Username</h1>
-            <label htmlFor="">Name: Alberto</label>
-            <label htmlFor="">Email: albertoarennass@gmail.com</label>
-            <label htmlFor="">Group Class: Majadahonda01</label>
+            <h1>{user.first_name}</h1>
+            <label htmlFor="">Email: {user.mail}</label>
+            <label htmlFor="">Group Class: {}</label>
             <button><Link to="/preferences">My Preferences</Link></button>
             <button> Search Group</button>
         </form>
